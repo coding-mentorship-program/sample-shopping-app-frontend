102226fcd6d9d787f10143a1d114ea55f441d93b
jonathans199 <jso1919@gmail.com>
2023-08-28 19:01:52 -0400

    added firebase configuration and script

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,25 +2,25 @@ import { useState, useEffect } from 'react'
 import { ProductCard } from '../components/ProductCard'
 
 const Home = () => {
-  const [products, setProducts] = useState([])
+	const [products, setProducts] = useState([])
 
-  useEffect(() => {
-    fetch(process.env.REACT_APP_API_ENDPOINT)
-      .then(res => res.json())
-      .then(data => setProducts(data))
-      .catch(err => console.error(err))
-  }, [])
+	useEffect(() => {
+		fetch(process.env.REACT_APP_API_ENDPOINT)
+			.then(res => res.json())
+			.then(data => setProducts(data))
+			.catch(err => console.error(err))
+	}, [])
 
-  const allProducts = products.map((product, index) => {
-    return <ProductCard key={product._id} product={product} index={index} /> 
-  })
+	const allProducts = products.map((product, index) => {
+		return <ProductCard key={product._id} product={product} index={index} />
+	})
 
-  return (
-    <div className='container'>
-      <h1>All Products</h1>
-      <div className='products'>{allProducts}</div>
-    </div>
-  )
+	return (
+		<div className='container'>
+			<h1>All Products</h1>
+			<div className='products'>{allProducts}</div>
+		</div>
+	)
 }
 
 export default Home
